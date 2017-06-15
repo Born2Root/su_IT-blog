333c01a669ca68b2b49e0af80b0ffdf785be3230
BO41 <lukasw41@gmail.com>
2017-06-15 13:30:27 +0200

    canvas depending on just two values:)

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -1,7 +1,11 @@
 var canvas = document.getElementById("cloud_canvas");
 var context = canvas.getContext("2d");
 
-var text = "test";
+var text = "test mal schauen";
+
+var x = 0;
+var y = 0;
+var text_height = 40;
 
 window.addEventListener("click", click, false);
 window.addEventListener('resize', resizeCanvas, false);
@@ -10,30 +14,32 @@ resizeCanvas();
 
 function redraw() {
 
-	/*
-		context.strokeStyle = "#367b44";
-		context.lineWidth = 5;
-		context.beginPath();
-		context.moveTo(45, 50);
-		context.lineTo(300, 90);
-		context.stroke();
-	*/
+	context.strokeStyle = "#367b44";
+	context.lineWidth = 5;
+
 	context.textAlign = "center";
-	context.font = "60px Georgia";
+	context.font = "40px Georgia";
 	context.fillStyle = "#ffffff";
 
-	var text_x = 100;
-	var text_y = 50;
-	var text_height = 40;
+	var text_x = 300;
+	var text_y = 70;
+
+
+	x = text_x - 5 - context.measureText(text).width / 2;
+	y = text_y - text_height + 5;
+
+
+	context.beginPath();
+	context.moveTo(text_x, text_y);
+	context.lineTo(300, 90);
+	context.stroke();
+
 
 	// x, y, x, y
-	context.fillRect(0, 10, 10 + context.measureText(text).width, 50);
+	context.fillRect(x, y, context.measureText(text).width + 10, text_height);
 	context.fillStyle = "#000000";
 	// x, y
 	context.fillText(text, text_x, text_y);
-
-
-
 }
 
 function resizeCanvas() {
@@ -51,7 +57,7 @@ function getMousePos(canvas, evt) {
 
 function click(e) {
 	var pos = getMousePos(canvas, e);
-	if (pos.x >= 0 && pos.x <= 10 + context.measureText(text).width && pos.y >= 10 && pos.y <= 40) {
-		alert("l");
+	if (pos.x >= x && pos.x <= x + context.measureText(text).width + 10 && pos.y >= y && pos.y <= y + text_height + 5) {
+		alert("link clicked!");
 	}
 }
