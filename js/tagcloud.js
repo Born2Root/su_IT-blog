23df2464cafb146184d1648640c814c052bd4b37
BO41 <lukasw41@gmail.com>
2017-06-15 12:37:50 +0200

    .

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -2,7 +2,6 @@ var canvas = document.getElementById("cloud_canvas");
 var context = canvas.getContext("2d");
 
 var text = "test";
-var text_height = 40;
 
 window.addEventListener("click", click, false);
 window.addEventListener('resize', resizeCanvas, false);
@@ -11,23 +10,27 @@ resizeCanvas();
 
 function redraw() {
 
-	context.strokeStyle = "#367b44";
-	context.lineWidth = 5;
-	context.beginPath();
-	context.moveTo(45, 50);
-	context.lineTo(300, 90);
-	context.stroke();
-
-
-
+	/*
+		context.strokeStyle = "#367b44";
+		context.lineWidth = 5;
+		context.beginPath();
+		context.moveTo(45, 50);
+		context.lineTo(300, 90);
+		context.stroke();
+	*/
 	context.textAlign = "center";
 	context.font = "60px Georgia";
 	context.fillStyle = "#ffffff";
+
+	var text_x = 100;
+	var text_y = 50;
+	var text_height = 40;
+
 	// x, y, x, y
 	context.fillRect(0, 10, 10 + context.measureText(text).width, 50);
 	context.fillStyle = "#000000";
 	// x, y
-	context.fillText(text, 45, 50);
+	context.fillText(text, text_x, text_y);
 
 
 
