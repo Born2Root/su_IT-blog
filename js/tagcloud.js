4fb460f3e117725efb3924070f81634d58ae5222
BO41 <lukasw41@gmail.com>
2017-06-15 17:50:32 +0200

    tags connect to neares neighbour

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -8,6 +8,14 @@ var tags = new Array({
 	x: 100,
 	y: 50
 }, {
+	text: "Memes",
+	x: 600,
+	y: 40
+}, {
+	text: "Websites",
+	x: 700,
+	y: 90
+}, {
 	text: "Blender",
 	x: 300,
 	y: 70
@@ -32,13 +40,23 @@ function redraw() {
 		var x = entry.x - 5 - context.measureText(entry.text).width / 2;
 		var y = entry.y - text_height + 5;
 
-		context.fillStyle = "#ffffff";
+		var result = nearest(entry)
+		var x_to = result.x;
+		var y_to = result.y - 10;
+
 		context.beginPath();
-		context.moveTo(entry.x, entry.y);
-		context.lineTo(300, 90);
+		context.moveTo(entry.x, entry.y - 10);
+		context.lineTo(x_to, y_to);
 		context.stroke();
 
+	});
 
+	tags.forEach(function (entry) {
+
+		var x = entry.x - 5 - context.measureText(entry.text).width / 2;
+		var y = entry.y - text_height + 5;
+
+		context.fillStyle = "#ffffff";
 		// x, y, x, y
 		context.fillRect(x, y, context.measureText(entry.text).width + 10, text_height);
 		context.fillStyle = "#000000";
@@ -73,5 +91,32 @@ function click(e) {
 			alert("link clicked: " + entry.text);
 		}
 	});
+
+}
+
+function nearest(tag) {
+	var x = 0;
+	var y = 0;
+	var diff_best = 1000;
+	var diff = 0;
+
+	tags.forEach(function (entry) {
+
+		if (entry.text != tag.text) {
+
+			diff = Math.abs(entry.x - tag.x) + Math.abs(entry.y - tag.y);
+			if (diff < diff_best) {
+				diff_best = diff;
+				x = entry.x;
+				y = entry.y;
+			}
+		}
+
+	});
+
+	return {
+		x: x,
+		y: y
+	};
 
 }
