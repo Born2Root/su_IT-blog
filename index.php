2bfc08cd08372c71871c1a06cb2026cea81bae87
BO41 <lukasw41@gmail.com>
2017-06-25 13:01:27 +0200

    added tags, but not happy with the look

--- index.php
+++ index.php
@@ -11,15 +11,20 @@ if(isset($_GET['type']) == false || $_GET['type']!='ajax'){
 
 	<div class="article" id="post_3" title="script_to_rice_your_setup_1.php">
 		<h2>Scripts To Rice Your Setup Pt.1</h2>
+		<img alt="tag" src="/icon/tag.svg" class="icon" />
+		<img alt="scripts" src="/icon/programming.svg" class="icon" />
 		<p>
-			First preview paragraph
+			Are you using a bash script for Linux that displays system information like Screenfetch or Archey, Screenfo or Neofetch?
+			Don't you get bored to see the same distro logo all the time?
+			<br/>
+			In this article, I'll show a little bash script on how to change this and rice your setup.
 		</p>
 	</div>
 
 	<div class="article" id="post_2" title="test_article.php">
 		<h2>Heading article 2</h2>
 		<p>
-			<img alt="macro" src="/macro1.jpg" /> Some other preview paragraph.
+			<img alt="macro" src="/macro1.jpg" class="img" /> Some other preview paragraph.
 		</p>
 	</div>
 
