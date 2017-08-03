3419fc16496bc842b63db1abc3dfa91b8eba44ec
BO41 <lukasw41@gmail.com>
2017-08-03 13:04:02 +0200

    added font awesome icons/font

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 	<meta name="robots" content="index, follow" />
 
 	<link rel="stylesheet" type="text/css" href="/css/main.css" />
+	<link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css" />
 
 </head>
 
@@ -25,18 +26,18 @@
 	<div id="header">
 
 		<canvas height="50" width="400" id="animation_canvas_header">
-			Your browser does not support canvas!
 		</canvas>
 
-		<img src="burger.png" alt="menu" class="icon" />
+		<i class="fa fa-bars" aria-hidden="true" id="burger"></i>
 
 		<div id="headline">
 			<a href="index.php" target="_self">
 				> su IT --blog
 			</a>
-			<!--span id="subtitle">- we sudo everything</span>
-			<span class="cursor">|</span> -
-			-->
+			<div style="display:none;">
+				<span id="subtitle">- we sudo everything</span>
+				<span class="cursor">|</span> -
+			</div>
 		</div>
 	</div>
 
@@ -49,6 +50,7 @@
 			<h1>Test article 1</h1>
 			<br>
 			<br>
+			<i class="fa fa-tags" aria-hidden="true"></i>:
 			<br>
 			<br>
 			<br>
@@ -91,11 +93,9 @@
 
 	<div id="footer">
 		<canvas height="50" width="400" id="animation_canvas_footer">
-			Your browser does not support canvas!
 		</canvas>
 		<div id="disclaimer">
             Disclaimer
-            <br/>
             <br/> &copy; 2017
         </div>
 	</div>
