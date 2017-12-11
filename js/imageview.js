4270d6089e58a2a7a6d81f5bb1a03b0d81efc402
BO41 <lukasw41@gmail.com>
2017-12-11 18:44:08 +0100

    imageview extracted

---
+++ js/imageview.js
@@ -0,0 +1,12 @@
+var modal = document.getElementById('myModal');
+
+modal.onclick = function () {
+	this.style.display = "none";
+}
+
+function show(img) {
+	var modalImg = document.getElementById("bigview");
+	var source = window.getComputedStyle(img, false).backgroundImage.slice(5, -2);
+	modal.style.display = "block";
+	modalImg.src = source;
+}
