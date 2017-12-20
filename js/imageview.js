a08ff7390dbdc2444e744a3fa79052936a97c086
BO41 <lukasw41@gmail.com>
2017-12-20 11:19:01 +0100

    ^ added image zoom to article images

--- js/imageview.js
+++ js/imageview.js
@@ -6,8 +6,7 @@ modal.onclick = function () {
 
 function show(img) {
 	var modalImg = document.getElementById("bigview");
-	var source = window.getComputedStyle(img, false).backgroundImage.slice(5, -2);
-	// ... | img.src;
+	var source = window.getComputedStyle(img, false).backgroundImage.slice(5, -2) || img.src;
 	modal.style.display = "block";
 	modalImg.src = source;
 }
